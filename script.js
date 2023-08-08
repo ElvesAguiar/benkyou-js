5a8c4f93dbc7096196cb61a278c24d1a5fcec324
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-08 12:59:08 -0300

    feat: Lab

--- script.js
+++ script.js
@@ -82,3 +82,39 @@ for (let i = 0; i < books.length; i++) {
 };
 
 console.log(sum);
+
+//------------------ LAB --------------------------------------------------------//
+let contacts = [
+    {
+        name: "Maxwell Wright",
+        phone: "(0191) 719 6495",
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    },
+    {
+        name: "Raja Villarreal",
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com"
+    },
+    {
+        name: "Helen Richards",
+        phone: "0800 1111",
+        email: "libero@convallis.edu"
+    }
+];
+
+let newContact = {
+    name: "Maisie Haley",
+    phone: "0913 531 3030",
+    email: "risus.Quisque@urna.ca"
+}
+contacts.push(newContact);
+
+console.log(`name: ${contacts[0].name} 
+phone: ${contacts[0].phone} 
+email: ${contacts[0].email}`);
+
+
+console.log(`name: ${contacts[contacts.length-1].name} 
+phone: ${contacts[contacts.length-1].phone} 
+email: ${contacts[contacts.length-1].email}`);
+
