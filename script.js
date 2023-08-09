51269234c43ad148c7aa6d4782cf445c466eb11d
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-09 17:01:53 -0300

    feat: loop for options

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 //------------------ LAB --------------------------------------------------------//
+
+
 let contacts = [
     {
         name: "Maxwell Wright",
@@ -20,39 +22,49 @@ let contacts = [
 
 let newContact = {
 }
+let isOver=true;
+while (isOver) {
+
+
+    console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+
+
+    console.log(`${contacts[contacts.length - 1].name} / ${contacts[contacts.length - 1].phone} / ${contacts[contacts.length - 1].email}`);
 
 
 
-console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+
+    let choice = prompt("Chose an option\n display the first contact (first) \n display the last contact (last) \n display all contacts (all) \n add a new contact (new) \n exit the program (quit)");
 
 
-console.log(`${contacts[contacts.length - 1].name} / ${contacts[contacts.length - 1].phone} / ${contacts[contacts.length - 1].email}`);
-
-
-
-
-let choice = Number(prompt("make a choice with the options \n 1-showing the first contact (first) \n 2-showing the last contact (last)\n 3-adding a new contact (new) "));
-
-
-switch (choice) {
-    case 1:
-        alert(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
-        break;
-    case 2:
-        alert(`${contacts[contacts.length - 1].name} / ${contacts[contacts.length - 1].phone} / ${contacts[contacts.length - 1].email}`);
-        break;
-    case 3:
-        newContact.name = window.prompt("name: ");
-        newContact.phone = window.prompt("phone: ");
-        newContact.email = window.prompt("email: ");
-
-        if ((String(newContact.name).length > 0 && newContact.name != null) && (String(newContact.phone).length > 0 && newContact.name != null) && (String(newContact.email).length > 0) && newContact.name != null) {
-            contacts.push(newContact);
+    switch (choice) {
+        case "first":
+            alert(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+            break;
+        case "last":
             alert(`${contacts[contacts.length - 1].name} / ${contacts[contacts.length - 1].phone} / ${contacts[contacts.length - 1].email}`);
-        };
+            break;
+        case "new":
+            newContact.name = window.prompt("name: ");
+            newContact.phone = window.prompt("phone: ");
+            newContact.email = window.prompt("email: ");
 
-        break;
-    default:
-        console.log("invalid!")
-        break;
+            if ((String(newContact.name).length > 0 && newContact.name != null) && (String(newContact.phone).length > 0 && newContact.name != null) && (String(newContact.email).length > 0) && newContact.name != null) {
+                contacts.push(newContact);
+                alert(`${contacts[contacts.length - 1].name} / ${contacts[contacts.length - 1].phone} / ${contacts[contacts.length - 1].email}`);
+            };
+            break;
+        case "quit":
+            isOver=false;
+            break;
+        case "all":
+            let text;
+            for(contact of contacts){
+                text+=`${contact.name} / ${contact.phone} / ${contact.email}\n`
+            }
+            alert(text);
+        default:
+            console.log("invalid!")
+            break;
+    }
 }
