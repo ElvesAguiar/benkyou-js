bb7d9d30291abea4ff855695051cf87d304a11b5
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-09 16:31:13 -0300

    feat: using loop while to make mani calculations

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-let firstNumber = Number(prompt("Enter first number"));
-let secondNumber = Number(prompt("Enter second number"));
-let operand = prompt("Enter operand (+, -, * or /)");
-let result;
+while (true) {
+    let firstNumber = prompt("Enter first number");
+    let secondNumber = prompt("Enter second number");
+    let operand = prompt("Enter operand (+, -, * or /)");
+    let result;
 
-if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
-    switch (operand) {
-        case "+": result = firstNumber + secondNumber; break;
-        case "-": result = firstNumber - secondNumber; break;
-        case "*": result = firstNumber * secondNumber; break;
-        case "/": result = firstNumber / secondNumber; break;
-        default: result = "Error: unknown operand";
+    if (firstNumber === "Q" || secondNumber === "Q" || operand === "Q") {
+        break;
     }
-} else {
-    result = "Error: at least one of the entered values is not a number";
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+        switch (operand) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                result = "Error: unknown operand";
+        }
+    } else {
+        result = "Error: at least one of the entered values is not a number";
+    }
+    alert(result);
 }
-alert(result);
