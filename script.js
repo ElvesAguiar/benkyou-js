10d7780fc194a8c6e6b3d03058bd3e85e6225072
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-11 17:50:31 -0300

    feat:using loop for to make Fibonacci

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let fibbRec = function (n) {
 
 console.log(fibbRec(4)); // -> 3
 console.log(fibbRec(7)); // -> 13
+
+
+function fibbIter(n) {
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a;
+        a = b;
+        b += c;        
+    }
+    return b;
+}
+
+console.log(fibbIter(4)); // -> 3
+console.log(fibbIter(7)); // -> 13
