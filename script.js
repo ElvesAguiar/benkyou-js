e0a79123114afde969a06d3c63101f2214fbfa30
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-11 17:16:43 -0300

    feat: Exercise 6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
-
-
-let counter = 1;
-let intervalId = setInterval(() => {
-    console.log(counter++);
-}, 2000);
-
-setTimeout(function () {
-    clearInterval(intervalId)
-}, 20000);
+let fibbRec = function (n) {
+    let retVal = 0;
+    if (n != 0) {
+        if (n === 1) {
+            retVal = 1;
+        } else {
+            retVal = fibbRec(n - 1) + fibbRec(n - 2);
+        }
+    }
+    return retVal;
+}
