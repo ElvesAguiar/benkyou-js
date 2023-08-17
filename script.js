6b5baed6683791a7a280e35b16d099ab1602f752
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-17 16:25:34 -0300

    feat: learning react

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-let fibbRec = function (n) {
-    let retVal = 0;
+let x =[0,1,2,3,4];
 
-    if (n !== 0) retVal = n === 1 ? 1 : fibbRec(n - 1) + fibbRec(n - 2);
+let a=x.slice(0,2);
 
-    return retVal;
-}
-
-console.log(fibbRec(4)); // -> 3
-console.log(fibbRec(7)); // -> 13
-
-
-function fibbIter(n) {
-    let a = 0, b = 1;
-    for (let i = 2; i <= n; i++) {
-        let c = a;
-        a = b;
-        b += c;        
-    }
-    return b;
-}
-
-console.log(fibbIter(4)); // -> 3
-console.log(fibbIter(7)); // -> 13
+console.log(a);
