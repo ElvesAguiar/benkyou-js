b156e5ff17543b1926a698b85d282b8f3e24701a
Elves Aguiar <elvesmhs@hotmail.com>
2023-08-18 09:27:29 -0300

    feat: local storage

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-function entrar(){
-    var area = document.getElementById('area');
-    var texto = prompt('digite seu nome?')
 
-    area.innerHTML = texto;
-}
 
-let array = [1,2,3,4,5];
 
-for(let i=0;i<array.length;i++){
-    console.log(array[i]);
-}
-console.log("\n");
-array.forEach((e)=>{console.log(e)})
-console.log("\n");
 
-for(let element of array){
-    console.log(element)
-}
+let pessoa = {
+    nome:"elves",
+    sobreNome:"Nogueira",
+    cargo:"programdaor"
+};
 
-function acao(){
-     document.write("Executando...<br/>");
-}
-var timer =setInterval(acao,2000);
+
+let {sobreNome}=pessoa;
+
+console.log(sobreNome);
+// console.log(cargo);
+
+// let {nome,cargo}=pessoa;
+
+// // console.log(nome);
+// // console.log(cargo);
+
+
+// // let {cargo:desenvolvedor}=pessoa;
+
+
+// // console.log(desenvolvedor);
+
 
